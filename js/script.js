66e8da9945a76fa6696194f7309fb33c52d58cb6
Dylan <Dylanjdaly2006@gmail.com>
2026-02-19 11:53:52 +0000

    Small change regarding slideUp and slideDown on HomePage

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 $(".card").mouseenter(function() {
 		$(this).addClass('highlight_option');
-  $(this).children().slideUp("slow");
+  $(this).children().slideDown("slow");
   $(this).siblings().fadeTo("fast", 0.1);
 	})
 $(".card").mouseleave(function() {
 		$(this).removeClass('highlight_option');
-  $(this).children().slideDown("slow");
+  $(this).children().slideUp("slow");
   $(this).siblings().fadeTo("fast", 1);
 	})
   $("label").hide();
