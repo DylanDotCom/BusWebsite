a85529f91d9eab16061644c3979befe956dd98a1
Dylan <Dylanjdaly2006@gmail.com>
2026-02-19 11:49:51 +0000

    Changed slide up and down targets

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 $(".card").mouseenter(function() {
 		$(this).addClass('highlight_option');
-  $(".card").next().slideDown("slow");
+  $(this).children("label").slideUp("slow");
   $(this).siblings().fadeTo("fast", 0.1);
 	})
 $(".card").mouseleave(function() {
 		$(this).removeClass('highlight_option');
-  $(".card").next().slideUp("slow");
+  $(this).children("label").slideDown("slow");
   $(this).siblings().fadeTo("fast", 1);
 	})
-
+  $("label").hide();
 
 });
