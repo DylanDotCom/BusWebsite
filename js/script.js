d32b650afe925e53d374d59b7c3a9105acfdfbcc
Dylan <Dylanjdaly2006@gmail.com>
2026-02-19 10:52:01 +0000

    Update script.js
    Started jQuery script to make the Home Page more responsive and visually appealing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+$(".card").mouseenter(function() {
+		$(this).addClass('highlight_option');
+  $(".card").next().slideDown("slow");
+  $(this).siblings().fadeTo("fast", 0.1);
+	})
+$(".card").mouseleave(function() {
+		$(this).removeClass('highlight_option');
+  $(".card").next().slideUp("slow");
+  $(this).siblings().fadeTo("fast", 1);
+	})
 
+
+});
