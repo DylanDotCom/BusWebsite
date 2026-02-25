dbce738234175ca533c95dac9dfbc1f1a92a280e
Dylan <Dylanjdaly2006@gmail.com>
2026-02-25 09:59:24 +0000

    Fixing and cleaning up homepage cards and links to other pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $(".card").children("div").hide();
 $(".card").mouseenter(function() {
 		$(this).addClass('highlight_option');
   $(this).children("div").slideDown("slow");
@@ -10,5 +11,16 @@ $(".card").mouseleave(function() {
   $(this).siblings().fadeTo("fast", 1);
 	})
   $("label").hide();
-
+$(".card1").on("click", function(){
+   window.location.href = "Newspage.html";
+})
+$(".card3").on("click", function(){
+   window.location.href = "TicketBooking.html";
+})
+$(".card4").on("click", function(){
+   window.location.href = "Safetypage.html";
+})
+$(".card5").on("click", function(){
+   window.location.href = "Bustimetablepage.html";
+})
 });
